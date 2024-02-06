665911be9176ca7543958a2deb18a1cca0260c11
Waqas Asif <waqas.a@d4interactive.io>
2024-02-06 16:59:04 +0500

    changing strapi version from 4.1.7 to 4.19.1

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,18 @@
 module.exports = ({ env }) => ({
+  playgroundAlways: true,
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
   upload: {
     config: {
       provider: "cloudinary",
