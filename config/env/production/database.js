6baaa687c7e1f66abda0d70e0f583cfd4d508bf9
Waqas Asif <waqas.a@d4interactive.io>
2024-02-07 15:43:12 +0500

    changing strapi version from 4.1.7 to 4.19.1

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,7 +2,7 @@ const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
+  console.log("final configuration", env("DATABASE_URL"));
   return {
     connection: {
       client: "postgres",
