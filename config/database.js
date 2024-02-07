689a7b49db741744f4e5a174c92013471ffd176c
Waqas Asif <waqas.a@d4interactive.io>
2024-02-07 17:02:47 +0500

    changing strapi version from 4.1.7 to 4.19.1

--- config/database.js
+++ config/database.js
@@ -1,15 +1,12 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-console.log("final configuration sqlite",  env('DATABASE_FILENAME', '.tmp/data.db'));
-console.log("final configuration sqlite 2",  path.join(__dirname, '..', '.tmp/data.db'));
 
   return ({
     connection: {
       client: 'sqlite',
       connection: {
-        // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-        filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true,
     },
